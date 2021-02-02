b53960ed332890334201728ed3108b89bd59bc9d
farouk afolabi <afolabifarouk99@gmail.com>
2021-02-02 15:51:09 +0300

    day 3

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,42 @@
-console.log('hello world'); 
+/*const btnHamburger = document.querySelector('#btnHarmburger');
+const header = document.querySelector('.header');
+
+
+
+btnHamburger.addEventListener('click', function(){
+console.log( 'click hamburger');
+if(header.classList.contains('open')){
+    header.classList.remove('open');
+}
+else {
+    header.classList.add('open');
+}
+});
+ */
+// const btnHamburger = document.querySelector("#btnHamburger");
+// const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function () {
+  // console.log("open hamburger");
+
+  if (header.classList.contains("open")) {
+    //close hamburger menu
+    // body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    // hamburger menu
+    // body.classList.add('noscroll');
+    header.classList.add('open');
+    fadElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
