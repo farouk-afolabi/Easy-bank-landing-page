92a01af39082b094df633314c942f19e6d02878f
farouk afolabi <afolabifarouk99@gmail.com>
2021-02-04 22:34:02 +0300

    day 4 debugging

--- app/js/script.js
+++ app/js/script.js
@@ -1,42 +1,24 @@
-/*const btnHamburger = document.querySelector('#btnHarmburger');
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
+btnHamburger.addEventListener('click', function() { // close menu
+    if (header.classList.contains('open')) {
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
 
-
-btnHamburger.addEventListener('click', function(){
-console.log( 'click hamburger');
-if(header.classList.contains('open')){
-    header.classList.remove('open');
-}
-else {
-    header.classList.add('open');
-}
-});
- */
-// const btnHamburger = document.querySelector("#btnHamburger");
-// const body = document.querySelector("body");
-const header = document.querySelector(".header");
-const overlay = document.querySelector(".overlay");
-const fadElems = document.querySelectorAll(".has-fade");
-
-btnHamburger.addEventListener("click", function () {
-  // console.log("open hamburger");
-
-  if (header.classList.contains("open")) {
-    //close hamburger menu
-    // body.classList.remove('noscroll');
-    header.classList.remove('open');
-    fadElems.forEach(function (element) {
-      element.classList.remove("fade-in");
-      element.classList.add("fade-out");
-    });
-  } else {
-    // hamburger menu
-    // body.classList.add('noscroll');
-    header.classList.add('open');
-    fadElems.forEach(function (element) {
-      element.classList.remove("fade-out");
-      element.classList.add("fade-in");
-    });
-  }
+    } else { // open menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
 });
